1ccf111d20086209a015d8aec8dbf7e12e2004a2
Caed <Oskar.dyla1999@gmail.com>
2026-09-06 01:32:55 +0200

    feat(routing): Added geometry to route

--- services/RoutingService/RoutingService/Domain/Route.cs
+++ services/RoutingService/RoutingService/Domain/Route.cs
@@ -5,4 +5,5 @@ public record Route
     public double DistanceMeters { get; init; }
     public double DurationSeconds { get; init; }
     public IReadOnlyList<Waypoint> Waypoints { get; init; } = [];
+    public string? Geometry { get; init; }
 }
--- services/RoutingService/RoutingService/Providers/OSRMRouteProvider.cs
+++ services/RoutingService/RoutingService/Providers/OSRMRouteProvider.cs
@@ -33,13 +33,14 @@ public class OsrmRoutingProvider : IRoutingProvider
         );
 
         var response = await _httpClient.GetFromJsonAsync<OsrmResponse>(
-            $"/route/v1/driving/{coordinates}?overview=false",
+            $"/route/v1/driving/{coordinates}?overview=full&geometries=polyline",
             cancellationToken
         );
 
         if (response is null || response.Routes.Count == 0)
         {
-            throw new InvalidOperationException("OSRM could not calculate a route.");
+            throw new InvalidOperationException(
+                "OSRM could not calculate a route.");
         }
 
         var route = response.Routes[0];
@@ -48,9 +49,13 @@ public class OsrmRoutingProvider : IRoutingProvider
         {
             DistanceMeters = route.Distance,
             DurationSeconds = route.Duration,
+
             Waypoints = locations
-                .Select((location, index) => new Waypoint(index.ToString(), location))
-                .ToList()
+                .Select((location, index) =>
+                    new Waypoint(index.ToString(), location))
+                .ToList(),
+
+            Geometry = route.Geometry
         };
     }
 
@@ -63,5 +68,6 @@ public class OsrmRoutingProvider : IRoutingProvider
     {
         public double Distance { get; set; }
         public double Duration { get; set; }
+        public string Geometry { get; set; } = "";
     }
 }
